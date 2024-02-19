e194e4a8e8c4e854295257f16fb9d056647f8ce0
Valentin Gonzalez <101377457+Tintaa307@users.noreply.github.com>
2024-02-18 23:16:43 -0300

    change some mx

--- src/components/console/ConsoleHeader.tsx
+++ src/components/console/ConsoleHeader.tsx
@@ -4,13 +4,13 @@ import React from "react"
 const ConsoleHeader = ({ fileName }: { fileName: string }) => {
   return (
     <nav className="w-full h-14 bg-[#080808] flex justify-between items-center border-b-[1px] border-b-white/10 rounded-t-3xl">
-      <small className="text-white/60 text-sm font-normal flex items-center flex-row gap-1 mx-20">
+      <small className="text-white/60 text-sm font-normal flex items-center flex-row gap-1 mx-14">
         <IconBrandReact size={18} className="text-blue-600" />
         {fileName}
       </small>
       <IconClipboardText
         size={20}
-        className="text-white/90 mx-20 cursor-pointer"
+        className="text-white/90 mx-14 cursor-pointer"
       />
     </nav>
   )
